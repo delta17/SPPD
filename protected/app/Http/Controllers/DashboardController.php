cb9481573d2523e546de8780d3fb1a2eabf00ef1
delta17 <13.7488@stis.ac.id>
2019-05-16 09:30:16 +0700

    fix dashboard

--- protected/app/Http/Controllers/DashboardController.php
+++ protected/app/Http/Controllers/DashboardController.php
@@ -9,16 +9,16 @@ class DashboardController extends Controller
 {
   public function showDashboard()
   {
-    $query = UserJLN::all()->groupBy('action')->get(2);
-    $count = $query->count();
-//    dd($query[0]->getUser->name." melakukan ".$query[0]->getUraianKegiatan->uraian);
-//    dd($query[0]->tgl_sampai);
-
-    $data = array();
-    for($i=0; $i<$count; $i++){
-      $title = $query[$i]->getUser->name." melakukan ".$query[$i]->getUraianKegiatan->uraian;
-      $data[] = array('title'=>$title,'start'=>$query[$i]->tgl_dari,'end'=>$query[$i]->tgl_sampai);
-    }
+//    $query = UserJLN::all()->groupBy('action')->get(2);
+//    $count = $query->count();
+////    dd($query[0]->getUser->name." melakukan ".$query[0]->getUraianKegiatan->uraian);
+////    dd($query[0]->tgl_sampai);
+//
+//    $data = array();
+//    for($i=0; $i<$count; $i++){
+//      $title = $query[$i]->getUser->name." melakukan ".$query[$i]->getUraianKegiatan->uraian;
+//      $data[] = array('title'=>$title,'start'=>$query[$i]->tgl_dari,'end'=>$query[$i]->tgl_sampai);
+//    }
 //    dd($data);
 //    $data[] = array('title'=>'Lorem ipsum dolor sit amet','start'=>$year.'-'.$month.'-01','className'=>'green');
 //    $data[] = array('title'=>'Lorem ipsum dolor sit amet','start'=>$year.'-'.$month.'-01','className'=>'green');
@@ -31,6 +31,6 @@ class DashboardController extends Controller
 //    $data[] = array('title'=>'Ut faucibus sapien','start'=>date("Y-m-d"));
 //    $data[] = array('title'=>'Donec porta orci dapibus','start'=>$year.'-'.$month.'-21','end'=>$year.'-'.$month.'-28','className'=>'blue');
 //    $data[] = array('title'=>'Phasellus ac arcu in tortor faucibus pharetra','start'=>$year.'-'.$month.'-21','end'=>$year.'-'.$month.'-25','className'=>'red');
-    return view('dashboard',compact('data'));
+    return view('dashboard');
   }
 }
